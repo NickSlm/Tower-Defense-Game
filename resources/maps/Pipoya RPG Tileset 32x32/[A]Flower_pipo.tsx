7415f84e17b26601b7f63996f1bb794e5d9c7449
NickSlm <solomonovnick@gmail.com>
2022-02-03 10:11:11 +0200

    Added tileset assets

---
+++ resources/maps/Pipoya RPG Tileset 32x32/[A]Flower_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="[A]Flower_pipo" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="../Pipoya RPG Tileset 32x32/SampleMap/[A]Flower_pipo.png" width="256" height="384"/>
+</tileset>
--- resources/maps/grass-tiles-2-small.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="grass-tiles-2-small" tilewidth="32" tileheight="32" tilecount="72" columns="12">
- <image source="../../../../tileset/grass-tiles-2-small.png" width="384" height="192"/>
-</tileset>
